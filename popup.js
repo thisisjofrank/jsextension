fe808d70503998eed6f4ef030b420e03c526b5fd
David Whitney <david@davidwhitney.co.uk>
2021-05-13 17:12:00 +0100

    Added variables for elements.

--- popup.js
+++ popup.js
@@ -1,13 +1,16 @@
-changeColor.addEventListener("click", async () => {
+const button = document.getElementById("changeColor");
+const picker = document.getElementById("color");
+
+button.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    chrome.storage.sync.set({ color: color.value });
+    chrome.storage.sync.set({ color: picker.value });
   
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: setPageBackgroundColor,
     });
-    
+
 });
   
 async function setPageBackgroundColor() {
