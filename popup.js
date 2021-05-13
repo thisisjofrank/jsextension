168b52dcde58b990bf0d1e14bc098be3ab9e7aa0
David Whitney <david@davidwhitney.co.uk>
2021-05-13 15:15:45 +0100

    Simplest possible chrome / edge extension.

---
+++ popup.js
@@ -0,0 +1,12 @@
+changeColour.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: setPageBackgroundColor,
+    });
+});
+  
+function setPageBackgroundColor() {
+    document.body.style.backgroundColor = "red";
+}
